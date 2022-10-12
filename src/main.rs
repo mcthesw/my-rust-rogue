5ec85ed8ee9a1a0ab6378a69a0a436f7780e2fca
Sworld <1628767411@qq.com>
2022-10-12 09:41:30 +0800

    玩家组件

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::{max, min};
+
 use rltk::{GameState, Rltk, RGB};
 use specs::prelude::*;
 use specs_derive::Component;
@@ -15,18 +17,46 @@ struct Renderable {
     bg: RGB,
 }
 
-#[derive(Component,Debug)]
-struct LeftMover{}
+#[derive(Component, Debug)]
+struct LeftMover {}
 
-struct LeftWorker{}
+struct LeftWorker {}
 impl<'a> System<'a> for LeftWorker {
-    type SystemData = (ReadStorage<'a,LeftMover>,
-                        WriteStorage<'a,Position>);
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
     fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_lefty,pos) in (&lefty,&mut pos).join(){
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
             pos.x -= 1;
-            if pos.x < 0{pos.x=79;}
+            if pos.x < 0 {
+                pos.x = 79;
+            }
         }
+    }
+}
+
+#[derive(Component, Debug)]
+struct Player {}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        // 遍历所有玩家组件
+        pos.x = (pos.x+delta_x).clamp(0, 79);
+        pos.y = (pos.y+delta_y).clamp(0, 49);
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            rltk::VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            rltk::VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            rltk::VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            rltk::VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
     }
 }
 
@@ -35,8 +65,8 @@ struct State {
 }
 
 impl State {
-    fn run_system(&mut self){
-        let mut lw = LeftWorker{};
+    fn run_system(&mut self) {
+        let mut lw = LeftWorker {};
         lw.run_now(&self.ecs);
         self.ecs.maintain();
     }
@@ -45,8 +75,9 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        
+
         self.run_system();
+        player_input(self, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -64,6 +95,7 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
 
     gs.ecs
         .create_entity()
@@ -73,6 +105,7 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(Player {})
         .build();
 
     for i in 0..11 {
@@ -84,8 +117,9 @@ fn main() -> rltk::BError {
                 fg: RGB::named(rltk::RED),
                 bg: RGB::named(rltk::BLACK),
             })
-            .with(LeftMover{})
+            .with(LeftMover {})
             .build();
     }
+
     rltk::main_loop(context, gs)
 }
